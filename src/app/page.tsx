b113e2fedaf1ed631b38508f527f8a3ad1de9c93
Sofia Bargues <barguessofia@gmail.com>
2024-02-04 18:21:24 +0000

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
-import {Label} from "@radix-ui/react-label";
-
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Textarea} from "@/components/ui/textarea";
+import {Label} from "@/components/ui/label";
 
 export default function HomePage() {
+  async function donate(fromData: FormData) {
+    "use server";
+    console.log(fromData);
+  }
+
   return (
-    <form>
-      <Label>
+    <form className=" m-auto grid max-w-96 gap-8 border p-6">
+      <Label className="grid gap-2">
         <span>Valor</span>
         <Input type="number" />
       </Label>
-      <Label>
+      <Label className="grid gap-2">
         <span>Tu mensaje en la donacion</span>
         <Textarea />
       </Label>
+      <Button type="submit">Send</Button>
     </form>
   );
 }
